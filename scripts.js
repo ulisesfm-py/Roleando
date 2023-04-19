c9face4d060e6a816bf6f38f197c7864c39e0544
ulisesfm-py <ulisesfm.py@gmail.com>
2023-04-19 16:13:58 +0200

    Added html, css and js files

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('gm-button').addEventListener('click', function () {
+    // Aquí puedes agregar el código para mostrar el formulario de contacto para Game Masters
+    alert('Aquí mostrarás el formulario para convertirse en Game Master');
+});
+
+document.getElementById('partida-button').addEventListener('click', function () {
+    // Aquí puedes agregar el código para mostrar el formulario de solicitud de partidas personalizadas
+    alert('Aquí mostrarás el formulario para solicitar una partida personalizada');
+});
